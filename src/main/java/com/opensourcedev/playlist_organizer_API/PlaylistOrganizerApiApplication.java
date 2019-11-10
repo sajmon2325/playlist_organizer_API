a44f909a53a1aeda17f573bcefc0dac063b3fd0b
Šimon <devtoolguy@gmail.com>
2019-11-10 21:00:47 +0100

    Project structure
    Created project structure and maven dependencies for project

---
+++ src/main/java/com/opensourcedev/playlist_organizer_API/PlaylistOrganizerApiApplication.java
@@ -0,0 +1,13 @@
+package com.opensourcedev.playlist_organizer_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylistOrganizerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaylistOrganizerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/opensourcedev/playlist_organizer_API/PlaylistOrganizerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opensourcedev.playlist_organizer_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistOrganizerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
