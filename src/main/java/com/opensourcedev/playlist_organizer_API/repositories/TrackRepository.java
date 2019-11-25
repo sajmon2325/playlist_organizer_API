1676bed17eb3f104464c2f4ac8874addd95dd116
Šimon <devtoolguy@gmail.com>
2019-11-25 21:40:20 +0100

    Track Service Implemented
    Track Service Implemented with all functionality

--- src/main/java/com/opensourcedev/playlist_organizer_API/repositories/TrackRepository.java
+++ src/main/java/com/opensourcedev/playlist_organizer_API/repositories/TrackRepository.java
@@ -1,6 +1,5 @@
 package com.opensourcedev.playlist_organizer_API.repositories;
 
-import com.opensourcedev.playlist_organizer_API.domain_model.Artist;
 import com.opensourcedev.playlist_organizer_API.domain_model.Track;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.repository.CrudRepository;
@@ -11,6 +10,5 @@ import org.springframework.stereotype.Repository;
 public interface TrackRepository extends CrudRepository<Track, Long> {
 
     Track findByTrackName(String trackName);
-    Track findByArtist(Artist artist);
 
 }
--- src/main/java/com/opensourcedev/playlist_organizer_API/services/TrackService.java
+++ src/main/java/com/opensourcedev/playlist_organizer_API/services/TrackService.java
@@ -3,4 +3,6 @@ package com.opensourcedev.playlist_organizer_API.services;
 import com.opensourcedev.playlist_organizer_API.domain_model.Track;
 
 public interface TrackService extends CrudInterface<Track, Long> {
+
+    Track findByTrackName(String trackName);
 }
